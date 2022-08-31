169ed18ca3ebaa8fa10af5e876e06cec6b1f905a
leonidasmaterial <andreleonedas@gmail.com>
2022-08-31 10:22:18 -0300

    exerciciosComLet

---
+++ js/index.js
@@ -0,0 +1,101 @@
+
+//alert("ola mundo");
+//alert("Tudo bem e com vc?");
+//window.alert("mesma coisa");
+
+//console.log("texte");
+//console.log("mais um texto escondido");
+
+//confirm("deseja cancelar");
+//window.confirm("mesma coisa");
+
+//prompt("coloque uma informação");
+//window.prompt("mesma coisa");
+
+/*
+Faça um programa que receba quatro números inteiros, calcule e mostre a soma
+desses números.
+
+let num1=prompt("Digite o primeiro numero");
+let num2=prompt("Digite o segundo numero");
+let num3=prompt("Digite o terceiro numero");
+let num4=prompt("Digite o quarto numero");
+num1=Number(num1);
+num2=Number(num2);
+num3=Number(num3);
+num4=Number(num4);
+let resul;
+resul=num1+num2+num3+num4;
+alert(resul);
+*/
+/*
+Faça um programa que receba três notas, calcule e mostre a média aritmética.
+
+let nota1=prompt("Digite a primeira nota: ");
+let nota2=prompt("Digite a segunda nota: ");
+let nota3=prompt("Digite a terceira nota: ");
+nota1=Number(nota1);
+nota2=Number(nota2);
+nota3=Number(nota3);
+let media;
+media=((nota1+nota2+nota3)/3);
+alert("A media de notas é "+media);
+*/
+/*
+let salario=prompt("Digite o salario:");
+salario=Number(salario);
+let aumento;
+aumento=((25/100)*salario);
+let novoSalario;
+novoSalario=salario+aumento;
+alert(`O novo salario é ${novoSalario} o aumento foi de ${aumento}`);
+*/
+/*
+let nota1 = prompt("Digite a primeira nota: ");
+let nota2 = prompt("Digite a segunda nota: ");
+let nota3 = prompt("Digite a terceira nota: ");
+nota1 = Number(nota1);
+nota2 = Number(nota2);
+nota3 = Number(nota3);
+let media;
+media = ((nota1 + nota2 + nota3) / 3);
+*/
+
+/*
+0 até 4 reprovado
+5 até 6 recuperação
+>6 aprovado
+   if /else if /else
+if(media>=0 && media<=4){
+    alert("Reprovado nota "+media);
+}else if(media>4 && media<=6){
+    alert("Recuperação nota "+media);
+}else{
+    alert("Aprovado nota "+media);
+}
+
+*/
+/* somente com duas if / else
+if(media>=6){
+    alert("Aprovado nota "+ media);
+}else{
+    alert("Reprovado nota "+ media);
+}
+*/
+
+/*
+somete o if
+
+if(media>=0 && media<=4){
+    alert("Reprovado nota "+ media);
+} if(media>4 && media<5){
+    alert("Recuperação nota "+ media);
+} if(media>=5 && media<=7){
+    alert("Tem como melhorar nota "+ media);
+}if(media>7){
+    alert("Aprovado"+ media);
+}
+*/
+
+
+
